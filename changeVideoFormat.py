ea956e93061cdc1e8c1cb3f0a229fe5ef32b7604
Yooo31 <mrts.yoan@gmail.com>
2023-01-28 18:45:02 +0100

    Convert video to vertical

---
+++ changeVideoFormat.py
@@ -0,0 +1,21 @@
+from moviepy.editor import *
+
+file = "/home/yoan/Documents/tiktok/test.mp4"
+
+clip = VideoFileClip(file)
+aspect_ratio = clip.aspect_ratio
+
+# Resize the video based on the width
+resized_clip = clip.resize(width=780)
+
+# Create a black background clip
+black_bg = ColorClip((780,1280), color=(0,0,0))
+
+# Position the resized video clip in the center of the black background
+final_clip = CompositeVideoClip([black_bg, resized_clip.set_pos(('center', 'center'))])
+final_clip = final_clip.set_duration(resized_clip.duration)
+
+# Write the final video to file
+final_clip.write_videofile("output.mp4")
+
+
