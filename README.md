fa2c6eb65801070a8874cc330d466c705004d2cb
Yooo31 <mrts.yoan@gmail.com>
2023-01-28 19:31:10 +0100

    Add file to add text

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # video_trim
 Add video, define duration, set title, get result
+
+
+pip install imageio
+sudo apt-get install imagemagick
---
+++ addText.py
@@ -0,0 +1,18 @@
+from moviepy.editor import *
+import os
+import shutil
+
+path_to_cut_folder = "cut"
+path_to_finished_folder = "finished"
+
+for i, file in enumerate(os.listdir(path_to_cut_folder)):
+    video_path = os.path.join(path_to_cut_folder, file)
+    video = VideoFileClip(video_path)
+    txt_clip = (TextClip("Partie {}".format(i+1), fontsize=24, color='white')
+                .set_position('bottom')
+                .set_duration(video.duration))
+    final_video = CompositeVideoClip([video, txt_clip])
+    final_video_path = os.path.join(path_to_finished_folder, file)
+    final_video.write_videofile(final_video_path)
+    shutil.move(final_video_path, final_video_path)
+
