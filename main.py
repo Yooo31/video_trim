ef810e2e7617a6e94739c1ada210dda631c61e52
Yooo31 <mrts.yoan@gmail.com>
2024-02-27 14:36:43 +0100

    Starting bot

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-from process import downloadVideo, changeVideoFormat, cutVideos, addText
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.utils.helpers import escape_markdown
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 
-print("Start downloading this video")
-title = downloadVideo.downloadVideo("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-print("Video downloaded")
+import os
+from dotenv import load_dotenv
 
-print("Start change video format")
-changeVideoFormat.changeFormat()
-print("Format is changed")
 
-print("Start cuting video")
-cutVideos.cutVideo(75)
-print("Video are cut")
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-print("Start add text to video")
-addText.addText()
-print(("Text added"))
+
+def start(update, context):
+  keyboard = [
+            [InlineKeyboardButton("⚙️ Paramétres ⚙️", callback_data="settings")],
+            [InlineKeyboardButton("🎬 Commencer 🎬", callback_data="starting")]
+  ]
+
+  reply_markup = InlineKeyboardMarkup(keyboard)
+  update.message.reply_text("Que voulez vous faire ?", reply_markup=reply_markup)
+
+def startButton(update, context):
+  query = update.callback_query
+
+  chat_id = query.message.chat_id
+  print(chat_id)
+
+  if query.data == "settings" :
+    print("Settings selected")
+
+  elif query.data == "starting" :
+    print("Starting selected")
+
+def main():
+  updater = Updater(BOT_TOKEN, use_context=True)
+
+  dp = updater.dispatcher
+
+  dp.add_handler(CommandHandler("start", start))
+  dp.add_handler(CallbackQueryHandler(startButton))
+
+  # dp.add_handler(MessageHandler(Filters.text, handle_message))
+  # dp.add_error_handler(error)
+
+  updater.start_polling()
+  updater.idle()
+
+print("Bot started")
+main()
